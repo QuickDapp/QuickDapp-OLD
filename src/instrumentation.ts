84fefc21a7ea5fbc7b456bc9a322af70374f07a5
Ramesh Nair <ram@hiddentao.com>
2024-12-29 19:46:53 +0000

    fix: instrumentation had stopped working

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { register } = await import('./src/instrumentation.node')
+    const { register } = await import('./instrumentation.node')
     await register(process.env.OTEL_SERVICE_NAME)
   }
 }
